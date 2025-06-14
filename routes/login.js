6cd05dd9a9218095a95fd7de188a6949c1f0bc53
ettisafxrup <ettisafrup@gmail.com>
2025-06-15 01:22:21 +0600

    updated, added name key

--- routes/login.js
+++ routes/login.js
@@ -3,13 +3,14 @@ const paidEmails = require("../data/mails.js")
 let result = {}
 
 function matchData(input) {
-  console.log(input.email)
+  let userName = paidEmails.find((user) => user.email == input.email)
+
   if (!input) {
     result = { match: false, message: "No email found." }
   }
 
   if (paidEmails.includes(input.email)) {
-    result = { match: true, message: "Email Found!" }
+    result = { match: true, name: userName, message: "Email Found!" }
   } else {
     result = {
       match: false,
